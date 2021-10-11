aef39bf9f3d4b74d4e4524bb1d91b3e5884d8a3c
Alex <alexdeassis7@gmail.com>
2021-10-11 09:56:35 -0300

    Clase1-CreacionDeProyecto + Hola Mundo

---
+++ AppHolaMundo/src/com/localpayment/Miapp.java
@@ -0,0 +1,21 @@
+package com.localpayment;
+
+public class Miapp {
+
+	// es un comentario: Los comentarios seran omitidos por el compilador
+	// tenemos dos tipos de comentarios
+	// comentario de linea
+
+	/*
+	 * Soy un comentario de bloque
+	 */
+
+	// el main es nuestro punto de inicio de ejecucion de la app
+	public static void main(String[] args) {
+		// mostramos un mensaje por la salida estandart (Output) de la pc
+		System.out.println("Hola Mundo Cruel!");
+
+		// para guardar los cambios presionarmo ctrl + s
+	}
+
+}
